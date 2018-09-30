90aece6530fd671243e7f3f565728bd58d4da92b
Georgia Hong <georgiahong@Georgias-MacBook-Pro.local>
2018-09-30 15:56:14 -0400

    Add more courses

--- Backend/src/mock_data.py
+++ Backend/src/mock_data.py
@@ -3,8 +3,14 @@ from models import Course, Requirement
 courses = [
     Course("Operating Systems", "CS", "350", "A course on the internals of the operating system"),
     Course("Concurrency", "CS",  "343", "A course on exceptions and advanced control flow"),
-    Course("User Interfaces", "CS", "CS349", "Students will build their own user interfaces"),
-    Course("Algorithms", "CS", "341", "A walkthrough of common computer science algorithms")
+    Course("User Interfaces", "CS", "349", "Students will build their own user interfaces"),
+    Course("Algorithms", "CS", "341", "A walkthrough of common computer science algorithms"),
+    Course("Astronomy", "SCI", "238", "A course in astronomy intended for Math, Eng and Sci students."),
+    Course("Environmental Geology", "SCI", "250", "An introduction to geological concepts."),
+    Course("Compilers", "CS", "444", "Learn about phases of compilation."),
+    Course("Real-time", "CS", "452", "Tools and techniques of real-time programming."),
+    Course("Intro to AI", "CS", "486", "An introduction to the concepts of Artificial Intelligence."),
+    Course("Cryptography", "ECE", "409", "Introduction to cryptology and computer security.")
 ]
 
 requirements = [
